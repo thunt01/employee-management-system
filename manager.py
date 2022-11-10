1ffbbc202f28117ce07437bad00546111febcf0a
thunt01 <thaddeushunt8@gmail.com>
2022-11-10 01:57:44 -0500

    Update manager.py

--- manager.py
+++ manager.py
@@ -2,20 +2,23 @@ import tkinker as tk
 from tkinter import filedialog, Text
 import os
 
-root = tk.Tk()
 
+
+root = tk.Tk()
 canvas = tk.Canvas(root, height=700, width =700, bg="white")
 canvas.pack()
+add = tk.button(root, text="Add Employee", padx=10,pady=5, bg="gray")
+sys = system()
+sys.load()
+sys.showEmployees()
+root.mainloop()
+sys.save()
 
 
 
 
-with open('save.txt', 'w') as save:
-    for employee in employees:
-        save.write(employee.show() + ';')
-
-class employee():
-    def __init__(self, id, pass, first, last, occ, level, past, xp ,contact, note):
+class employee:
+    def __init__(self, id, passW, first, last, occ, level, past, xp ,contact, note):
         #self.id = id
         #self.password = pass
         #self.firstName = first
@@ -31,7 +34,7 @@ class employee():
         notes.append(note)
 
         self.info.append(id)
-        self.info.append(pass)
+        self.info.append(passW)
         self.info.append(first)
         self.info.append(last)
         self.info.append(occ)
@@ -42,7 +45,7 @@ class employee():
         self.info.append(notes)
 
         #add
-    def updateAll(self, pass, first, last, occ, level, past, xp ,contact):
+    def updateAll(self, passW, first, last, occ, level, past, xp ,contact):
         #self.password = pass
         #self.firstName = first
         #self.lastName = last
@@ -51,7 +54,7 @@ class employee():
         #self.pastExperience = past
         #self.timeInPos = xp
         #self.contactInfo: = contact
-        self.info[1] = pass
+        self.info[1] = passW
         self.info[2] = first
         self.info[3] = last
         self.info[4] = occ
@@ -99,8 +102,8 @@ class employee():
     def setID(self,id):
         self.id = id
 
-    def setPassword(self, pass):
-        self.password = pass
+    def setPassword(self, passW):
+        self.password = passW
 
     def setFirstName(self, fName):
         self.firstName = fName
@@ -127,32 +130,50 @@ class employee():
         self.notes = note
 
 
-
-class system():
+class system:
     def __init__(self):
-        searchBy = 0
-        sortBy = 0
-        employees = []
+        self.searchBy = 0
+        self.sortBy = 0
+        self.employees = []
+
+    def load(self):
         if os.path.isfile('save.txt'):
             with open('save.txt') as save:
                 empList = save.read()
                 empList = empList.split(';')
                 for emp in empList:
                     data = emp.split(',')
-                    self.employees.append(employee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8] data[9])
+                    newEmp = employee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9])
+                    self.employees.append(newEmp)
 
-    def checkAccess(self):
+    #def checkAccess(self):
 
-    def search(): employee[]
+    #def search(): employee[]
 
-    def sort(): employee[]
+    #def sort(): employee[]
 
-    def showEmployees(self)
-        print(employees) #adapt to gui
+    def save(self):
+        with open('save.txt', 'w') as save:
+            for employee in self.employees:
+                save.write(employee.show() + ';')
+
+    def showEmployees(self):
+        for employee in self.employees: #adapt to gui
+            empframe = tk.Frame(root)
+            empframe.pack()
+            L = Label(empframe, text= employee.show())
+            L.pack()
+            delete = tk.button(root, text="Delete", padx=10,pady=5, bg="gray")
+            delete.pack(side = RIGHT)
+            update = tk.button(root, text="Update", padx=10,pady=5, bg="gray")
+            update.pack(side = RIGHT)
+            addNote = tk.button(root, text="Note", padx=10,pady=5, bg="gray")
+            addNote.pack(side = RIGHT)
+
 
     def addEmployee(self, employee):
         employees.append(employee)
 
-    def removeEmployee(): void
+    #def removeEmployee(): void
 
-    def updateEmployee(): void
+    #def updateEmployee(): void
